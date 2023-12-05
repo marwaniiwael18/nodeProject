bff7a71e3a5f98b6cc86a65eb30bb1ea23fa6f21
unknown <mehdy.benromdhane@esprit.tn>
2023-12-06 00:34:40 +0100

    fixing test

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ server.use("/api", productRoutes); //CRUD routes
 
 
 
-const PORT = 5001;
+const PORT = 5000;
 server.listen(PORT, function () {
   console.log("Server is running on port:  " + PORT);
 });
