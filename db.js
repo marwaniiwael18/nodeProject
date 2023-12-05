d8f1700f481a0460cbecc32df4438c48a1bbfee3
unknown <mehdy.benromdhane@esprit.tn>
2023-12-05 14:07:49 +0100

    CRUD Product

---
+++ db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const uri = "mongodb://root:example@db:27017/";
+const connectDB = async () => {
+    try {
+        await mongoose.connect(uri , 
+        {useNewUrlParser: true, 
+        useUnifiedTopology: true
+    });
+    console.log("MongoDB Connected")
+    }
+    catch (error) {
+        console.error(error.message);
+        process.exit(1);
+    } 
+}
+
+module.exports = connectDB;
--- dockerfile
+++ dockerfile
@@ -1,11 +1,6 @@
-FROM node:10-alpine
-COPY package*.json /app/
-COPY index.js /app/
-
-
+FROM node:16-alpine
 WORKDIR /app
+COPY . /app
 RUN npm install
-
-EXPOSE 8085
-
-CMD [ "node", "index.js" ]
+EXPOSE 5000
+CMD ["npm", "start"]
