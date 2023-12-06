82aee032b2ba740fd9bcbb7cc41f21dfaa52ccb9
unknown <mehdy.benromdhane@esprit.tn>
2023-12-06 12:46:21 +0100

    change timeout

--- test/product.test.js
+++ test/product.test.js
@@ -13,15 +13,7 @@ const mongoose = require('mongoose');
 chai.use(chaiHttp);
 
 
-// beforeAll(async () => {
-//     // Connect to the test database before running tests
-//     await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-//   });
-  
-//   afterAll(async () => {
-//     // Disconnect from the test database after tests
-//     await mongoose.disconnect();
-//   });
+
 
 describe('Test', () => {
 
